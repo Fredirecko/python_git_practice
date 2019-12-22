ee1abf7599d1d9aee66581a272aa6581079c767b
Fredirecko <avspedaliere@gmail.com>
2019-12-22 09:30:00 -0800

    variable/array practice

---
+++ practice.py
@@ -0,0 +1,42 @@
+
+
+age = 35
+pi = 3.14
+five = "5"
+
+name = "My name is Indiana Jones"
+
+
+print(age*pi)
+print (name[0:7])
+print (five + ' ' + name)
+
+
+#! Lists use square brackets. If you want to perform arithmetic on a list you should be using an array instead
+myList = ["Rob", "Kirsten", "Tommy", "Ralphie", 5]
+
+
+myList2 = [1,2,3,4,5,6,7,8,9,10]
+
+print(myList2*3)
+
+
+print (myList)
+print (myList[2])
+
+#! Tuples use paranthesis instead of square brackets
+myTuple = (1,2,3,4)
+
+print(myTuple)
+
+#! Dictionary
+
+myDictionary = {}
+myDictionary["dark"] = "Darth Vader"
+myDictionary["light"] = "Luke Skywalker"
+myDictionary["Luke Saber"] = "Green"
+myDictionary["Vader's Saber"] = "Red"
+
+print(myDictionary)
+print(myDictionary.keys())
+
